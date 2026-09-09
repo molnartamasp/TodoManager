9c7c7f4dcbe5ce58a87ff6fa298a8d820747b7ed
molnartamasp <moltomi04@gmail.com>
2026-09-09 10:46:40 +0200

    hibás

---
+++ TodoManager/todoManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoManager
+{
+    public class todoManager
+    {
+
+        List<Todo> todos = new List<Todo>();
+
+        public TodoManager() { }
+
+        public void Add(Todo todo)
+        {
+            todos.Add(todo);
+        }
+
+        public void Remove(Todo todo) 
+        {
+            todos.Remove(todo);
+        }
+
+        public void DisplayAll()
+        {
+            foreach (var todo in todos)
+            {
+                Console.WriteLine($"{todo.Name}");
+            }
+        }
+    
+    }
+}
